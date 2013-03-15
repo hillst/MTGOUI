85b6e7ad91cb5de42572a2ceaef6700aa9082563
Steve <hillst@onid.orst.edu>
2013-03-15 14:20:59 -0700

    Moving the commit to head i guess. No idea what this will do because I'm not on a branch..

--- decks/editdeck.php
+++ decks/editdeck.php
@@ -1,4 +1,4 @@
-<?php include '../header.php'?>
+<?php include '../header.php' ?>
 <link rel="stylesheet" href="../my.css" />
 <script>
   $(function() {
@@ -10,17 +10,28 @@
             <div data-role="content">
                 <div class="ui-grid-a">
                     <div class="ui-block-a">
-                    <button data-inline="true" data-mini="true" color='blue'>R</button><button data-mini="true" data-inline="true" >B</button><button data-inline="true" data-mini="true">U</button><button data-inline="true" data-mini="true">W</button><button data-inline="true" data-mini="true">G</button>
                     <h2>Search your collection</h2>
-                    <input id="searchinput1" placeholder="Lightn" value="" type="search">
+                    <img title="Not-implemented: Click one of the colors to filter out cards of that color." src = '../images/5colors.png' class = 'filter' style = "width:30%; margin-top:-5px; float:right;"></img>
+                    
+                    <input id="searchinput1" placeholder="Lightn" value="" type="search" style="width:20%;">
                     <p/>
+                    <img src = "../images/fold.png" id ="fold"></img>
                     <img class = 'card' src="../images/card1.png">
                     <img class = 'card' src="../images/card2.png">
                     <img class = 'card' src="../images/card3.png">
                     <img class = 'card' src="../images/card4.png">
 					</div>
-					sdfghsdfg
                     <div class="ui-block-b" id = 'rightside'>
+                    <button data-inline="true">Save</button><button  data-inline="true">Deck Statistics</button>
+                    	<div data-inline="true" style="width: 50%; float:right; margin-top:7px;" data-role="fieldcontain">
+							<select name="select-choice-1" id="select-choice-1">
+								<option value="standard">Sort By: Color</option>
+								<option value="rush">Sort By: Converted Mana Cost</option>
+								<option value="express">Sort By: Card Type</option>
+							</select>
+						</div>
+						<div id="picture"></div>
+                    
                     </div>
                 </div>
             </div>
